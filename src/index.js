203691658b0b38a0a73568f010ffeadfc28db444
Ashish Agrawal <ashishagrawal979@gmail.com>
2020-02-07 15:36:24 +0530

    add redux logger

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import ListReducer from "./redux/LIstReducers";
 import ShoppingListContainer from "./components/ShoppingListContainer";
+import logger from 'redux-logger';
 
-const store = createStore(ListReducer);
+const store = createStore(ListReducer, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store={store}><ShoppingListContainer/></Provider>, document.getElementById('root'));
 
