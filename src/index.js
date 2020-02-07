89dcc17a374eb93251a49c1169c7138b0b3202a6
Ashish Agrawal <ashishagrawal979@gmail.com>
2020-02-07 16:13:01 +0530

    Add compose and redux dev tools

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
 import ListReducer from "./redux/LIstReducers";
 import ShoppingListContainer from "./components/ShoppingListContainer";
 import logger from 'redux-logger';
 
-const store = createStore(ListReducer, applyMiddleware(logger));
+const store = createStore(ListReducer, compose(applyMiddleware(logger), window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f=>f));
 
 ReactDOM.render(<Provider store={store}><ShoppingListContainer/></Provider>, document.getElementById('root'));
 
