3a7d1217dcc93dda1c55a277549ece52ffdfe933
Ashish Agrawal <ashishagrawal979@gmail.com>
2020-02-08 10:05:00 +0530

    add more tests, indentation fix

--- src/__tests__/components/ListItem.test.js
+++ src/__tests__/components/ListItem.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
@@ -11,36 +11,36 @@ Enzyme.configure({ adapter: new Adapter() });
 let [removeListItem] = new Array(1).fill(jest.fn());
 
 function shallowSetup() {
-    const props = {
-        item: {
-            id: 1,
-            date: new Date(),
-            name: 'a',
-            description: 'some description',
-            quantity: 10
-          },
-        key: 1,
-        removeListItem
-    };
-    // wrapper instance around rendered output
-    // const enzymeWrapper = shallow(<ListItem {...props} />);
-    const enzymeWrapper = mount(<ListItem {...props} />);
-    return {
-        props,
-        enzymeWrapper
-      };
+  const props = {
+    item: {
+      id: 1,
+      date: new Date(),
+      name: 'a',
+      description: 'some description',
+      quantity: 10
+      },
+    key: 1,
+    removeListItem
+  };
+  // wrapper instance around rendered output
+  // const enzymeWrapper = shallow(<ListItem {...props} />);
+  const enzymeWrapper = mount(<ListItem {...props} />);
+  return {
+    props,
+    enzymeWrapper
+  };
 }
 
 describe('Shallow rendered ListItem', () => {
-    it('should render ListItem', () => {
-      // Setup wrapper and assign props.
-      const { enzymeWrapper, props } = shallowSetup();
- 
-      expect(enzymeWrapper.find('.float-left').text()).toBe(props.item.name);
-      expect(enzymeWrapper.find('button.pull-right.btn.btn-danger').text()).toBe('Remove');
-      expect(enzymeWrapper.containsMatchingElement(<button type="submit" className="pull-right btn btn-danger">Remove</button>)).toBe(true);
+  it('should render ListItem', () => {
+    // Setup wrapper and assign props.
+    const { enzymeWrapper, props } = shallowSetup();
 
-      enzymeWrapper.find('[type="submit"]').simulate('submit', { preventDefault () {} })
-      expect(props.removeListItem.mock.calls.length).toBe(1)
-    });
+    expect(enzymeWrapper.find('.float-left').text()).toBe(props.item.name);
+    expect(enzymeWrapper.find('button.pull-right.btn.btn-danger').text()).toBe('Remove');
+    expect(enzymeWrapper.containsMatchingElement(<button type="submit" className="pull-right btn btn-danger">Remove</button>)).toBe(true);
+
+    enzymeWrapper.find('[type="submit"]').simulate('submit', { preventDefault () {} })
+    expect(props.removeListItem.mock.calls.length).toBe(1)
   });
+});
--- src/__tests__/reducers/ListReducers.test.js
+++ src/__tests__/reducers/ListReducers.test.js
@@ -1,46 +1,55 @@
 import ListReducers from '../../redux/LIstReducers';
 
-
 describe('list Reducer', () => {
    
   
-    it('returns the initial state when an action type is not passed', () => {
-        let initialState = {
-            items: {}
-        }
-      const reducer = ListReducers(undefined, {});
-  
-      expect(reducer).toEqual(initialState);
-    });
+  it('returns the initial state when an action type is not passed', () => {
+    let initialState = {
+      items: {}
+    }
+    const reducer = ListReducers(undefined, {});
+    expect(reducer).toEqual(initialState);
+  });
 
     it('should be able to add state when an action type is ADD_ITEM', () => {
-        let initialState = {
-            items: {}
-        }
+      let initialState = {
+        items: {}
+      }
 
-        let item = {
-            id: 1,
-            date: new Date(),
-            name: 'a',
-            description: 'description',
-            quantity: 1
-          };
+      let item = {
+        id: 1,
+        date: new Date(),
+        name: 'a',
+        description: 'description',
+        quantity: 1
+      };
 
       const reducer = ListReducers(initialState, { type : "add_item", item : item});
-        let expected = {
-            items : { "1":item}
-        }
+      let expected = {
+        items : { "1": item }
+      }
       expect(reducer).toEqual(expected);
     });
+
     it('should be able to remove state when an action type is REMOVE_ITEM', () => {
-        let initialState = {
-            items: { "1": {},"2":{}, "3": {}}
-        }
-        const reducer = ListReducers(initialState, { type : "remove_item", id:2});
-        let expected = {
-            items: { "1": {},"3": {}}
-        }
-        expect(reducer).toEqual(expected);
+      let initialState = {
+        items: { "1": {},"2":{}, "3": {}}
+      }
+      const reducer = ListReducers(initialState, { type : "remove_item", id:2});
+      let expected = {
+        items: { "1": {},"3": {}}
+      }
+      expect(reducer).toEqual(expected);
     });
 
+    it('should be able to remove all items when an action type is REMOVE_ALL_ITEMS', () => {
+      let initialState = {
+        items: { "1": {},"2":{}, "3": {}}
+      }
+      const reducer = ListReducers(initialState, { type : "remove_all_items"});
+      let expected = {
+        items: {}
+      }
+      expect(reducer).toEqual(expected);
+    });
   });
